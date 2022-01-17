573b34ec983be46e237ef0bd4b3b04db8d339d60
Sirawit.TN <96248579+sirawit-tn@users.noreply.github.com>
2022-01-17 16:56:52 +0700

    Add project files.

---
+++ Farmnet Service/MainFrm.Designer.cs
@@ -0,0 +1,196 @@
+﻿
+namespace Farmnet_Service
+{
+    partial class MainFrm
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainFrm));
+            this.notify = new System.Windows.Forms.NotifyIcon(this.components);
+            this.mainMenu = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.startToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.configToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.btnStop = new System.Windows.Forms.Button();
+            this.inputJSFile = new System.Windows.Forms.TextBox();
+            this.btnBrowse = new System.Windows.Forms.Button();
+            this.openFileDialog = new System.Windows.Forms.OpenFileDialog();
+            this.btnLog = new System.Windows.Forms.Button();
+            this.process = new System.Diagnostics.Process();
+            this.mainMenu.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // notify
+            // 
+            this.notify.BalloonTipIcon = System.Windows.Forms.ToolTipIcon.Info;
+            this.notify.ContextMenuStrip = this.mainMenu;
+            this.notify.Icon = ((System.Drawing.Icon)(resources.GetObject("notify.Icon")));
+            this.notify.Text = "Farmnet Service";
+            this.notify.Visible = true;
+            // 
+            // mainMenu
+            // 
+            this.mainMenu.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.startToolStripMenuItem,
+            this.configToolStripMenuItem,
+            this.exitToolStripMenuItem});
+            this.mainMenu.Name = "mainMenu";
+            this.mainMenu.Size = new System.Drawing.Size(111, 70);
+            // 
+            // startToolStripMenuItem
+            // 
+            this.startToolStripMenuItem.Checked = true;
+            this.startToolStripMenuItem.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.startToolStripMenuItem.Name = "startToolStripMenuItem";
+            this.startToolStripMenuItem.Size = new System.Drawing.Size(110, 22);
+            this.startToolStripMenuItem.Text = "Start";
+            // 
+            // configToolStripMenuItem
+            // 
+            this.configToolStripMenuItem.Name = "configToolStripMenuItem";
+            this.configToolStripMenuItem.Size = new System.Drawing.Size(110, 22);
+            this.configToolStripMenuItem.Text = "Config";
+            this.configToolStripMenuItem.Click += new System.EventHandler(this.configToolStripMenuItem_Click);
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(110, 22);
+            this.exitToolStripMenuItem.Text = "Exit";
+            this.exitToolStripMenuItem.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(12, 123);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(84, 42);
+            this.btnStart.TabIndex = 16;
+            this.btnStart.Text = "START";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // btnStop
+            // 
+            this.btnStop.Location = new System.Drawing.Point(149, 123);
+            this.btnStop.Name = "btnStop";
+            this.btnStop.Size = new System.Drawing.Size(84, 42);
+            this.btnStop.TabIndex = 17;
+            this.btnStop.Text = "STOP";
+            this.btnStop.UseVisualStyleBackColor = true;
+            this.btnStop.Click += new System.EventHandler(this.btnStop_Click);
+            // 
+            // inputJSFile
+            // 
+            this.inputJSFile.Location = new System.Drawing.Point(12, 25);
+            this.inputJSFile.Name = "inputJSFile";
+            this.inputJSFile.ReadOnly = true;
+            this.inputJSFile.Size = new System.Drawing.Size(352, 23);
+            this.inputJSFile.TabIndex = 18;
+            // 
+            // btnBrowse
+            // 
+            this.btnBrowse.Location = new System.Drawing.Point(149, 54);
+            this.btnBrowse.Name = "btnBrowse";
+            this.btnBrowse.Size = new System.Drawing.Size(84, 42);
+            this.btnBrowse.TabIndex = 19;
+            this.btnBrowse.Text = "Browse";
+            this.btnBrowse.UseVisualStyleBackColor = true;
+            this.btnBrowse.Click += new System.EventHandler(this.btnBrowse_Click);
+            // 
+            // openFileDialog
+            // 
+            this.openFileDialog.FileName = "Select JS File";
+            this.openFileDialog.Filter = "*.js|*.js";
+            this.openFileDialog.Title = "FarmNet Service";
+            // 
+            // btnLog
+            // 
+            this.btnLog.Location = new System.Drawing.Point(280, 123);
+            this.btnLog.Name = "btnLog";
+            this.btnLog.Size = new System.Drawing.Size(84, 42);
+            this.btnLog.TabIndex = 20;
+            this.btnLog.Text = "Log";
+            this.btnLog.UseVisualStyleBackColor = true;
+            this.btnLog.Click += new System.EventHandler(this.btnLog_Click);
+            // 
+            // process
+            // 
+            this.process.StartInfo.Domain = "";
+            this.process.StartInfo.FileName = "cmd.exe";
+            this.process.StartInfo.LoadUserProfile = false;
+            this.process.StartInfo.Password = null;
+            this.process.StartInfo.StandardErrorEncoding = null;
+            this.process.StartInfo.StandardInputEncoding = null;
+            this.process.StartInfo.StandardOutputEncoding = null;
+            this.process.StartInfo.UserName = "";
+            this.process.StartInfo.WindowStyle = System.Diagnostics.ProcessWindowStyle.Hidden;
+            this.process.SynchronizingObject = this;
+            // 
+            // MainFrm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(376, 181);
+            this.Controls.Add(this.btnLog);
+            this.Controls.Add(this.btnBrowse);
+            this.Controls.Add(this.inputJSFile);
+            this.Controls.Add(this.btnStop);
+            this.Controls.Add(this.btnStart);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "MainFrm";
+            this.Text = "Browse";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.MainFrm_FormClosing);
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.MainFrm_FormClosed);
+            this.Load += new System.EventHandler(this.MainFrm_Load);
+            this.Resize += new System.EventHandler(this.MainFrm_Resize);
+            this.mainMenu.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.NotifyIcon notify;
+        private System.Windows.Forms.ContextMenuStrip mainMenu;
+        private System.Windows.Forms.ToolStripMenuItem startToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem configToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.Button btnStop;
+        private System.Windows.Forms.TextBox inputJSFile;
+        private System.Windows.Forms.Button btnBrowse;
+        private System.Windows.Forms.OpenFileDialog openFileDialog;
+        private System.Windows.Forms.Button btnLog;
+        private System.Diagnostics.Process process;
+    }
+}
+
---
+++ Farmnet Service/MainFrm.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Farmnet_Service
+{
+    public partial class MainFrm : Form
+    {
+        public MainFrm()
+        {
+            InitializeComponent();
+        }
+
+        private void MainFrm_Load(object sender, EventArgs e)
+        {
+            inputJSFile.Text = Properties.Settings.Default.JSFILE;
+        }
+
+        private void LogDsp_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            e.Cancel = true;
+        }
+
+        private void MainFrm_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            notify.Visible = false;
+        }
+
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (MessageBox.Show(this, "You want to close Framnet Service?", "Confirm!", MessageBoxButtons.OKCancel) == DialogResult.OK)
+            {
+                Application.Exit();
+            }
+        }
+
+        private void MainFrm_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (MessageBox.Show(this, "You want to close Framnet Service?", "Confirm!", MessageBoxButtons.OKCancel) == DialogResult.OK)
+            {
+                Application.Exit();
+            }
+            else
+            {
+                e.Cancel = true;
+                this.WindowState = FormWindowState.Minimized;
+            }
+        }
+
+        private void configToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.Show();
+            this.WindowState = FormWindowState.Normal;
+        }
+
+        private void MainFrm_Resize(object sender, EventArgs e)
+        {
+            switch (this.WindowState)
+            {
+                case FormWindowState.Minimized:
+                    this.ShowInTaskbar = false;
+                    this.Hide();
+                    notify.Visible = true;
+                    notify.ShowBalloonTip(2000, "Farmnet Service", "Farmnet Service is running in background", ToolTipIcon.Info);
+                    break;
+                case FormWindowState.Normal:
+                    notify.Visible = false;
+                    this.ShowInTaskbar = true;
+                    break;
+            }
+        }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (File.Exists(inputJSFile.Text))
+                {
+                    process.StartInfo.Arguments = "/C nodemon " + inputJSFile.Text;
+                    process.Start();
+                    btnStop.Enabled = true;
+                    btnStart.Enabled = false;
+                }
+                else
+                {
+                    MessageBox.Show(this, "JS File not exists!", "Farmnet Service", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(this, ex.Message, "Farmnet Service", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+
+        }
+
+        private void btnBrowse_Click(object sender, EventArgs e)
+        {
+            if ((openFileDialog.ShowDialog()) == DialogResult.OK)
+            {
+                inputJSFile.Text = openFileDialog.FileName;
+                Properties.Settings.Default.JSFILE = inputJSFile.Text;
+                Properties.Settings.Default.Save();
+            }
+        }
+
+        private void btnLog_Click(object sender, EventArgs e)
+        {
+            //if (logDsp.Visible)
+            //{
+            //    logDsp.Focus();
+            //}
+            //else
+            //{
+            //    logDsp.Show();
+            //}
+            process.StartInfo.WindowStyle = ProcessWindowStyle.Normal;
+        }
+
+        private void btnStop_Click(object sender, EventArgs e)
+        {
+            if (process != null)
+            {
+                process.Close();
+                process = null;
+                btnStart.Enabled = true;
+                btnStop.Enabled = false;
+            }
+        }
+    }
+}
